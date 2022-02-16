7750c665e1f67faab9ca8ee9be96492699d5a056
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-16 17:39:30 -0600

    Importing reuseable code
    This code doesn't change between programs so we're going to use it again.

--- logsearch.py
+++ logsearch.py
@@ -1,1 +1,12 @@
 #this will be the main file
+import urllib.request
+import os.path
+#checks to see if the file exists, if it does NOT exist, download it and countine running.
+if not(os.path.isfile('awslog.txt')):  
+    print('You do not have the file, beginning file download with urllib2...')
+    url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+    urllib.request.urlretrieve(url, 'awslog.txt')
+    print('File downloaded, resuming program.')
+    
+# Opening a file
+file = open("awslog.txt","r")
