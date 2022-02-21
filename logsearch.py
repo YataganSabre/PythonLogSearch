861d6890d68dfa25acf69dd38c4110904029ed5e
amanvad <99163608+amanvad@users.noreply.github.com>
2022-02-21 11:10:22 -0600

    Update logsearch.py

--- logsearch.py
+++ logsearch.py
@@ -1,7 +1,8 @@
-#this will be the main file
+# This will be the main file
 import urllib.request
 import os.path
-#checks to see if the file exists, if it does NOT exist, download it and countine running.
+
+# Checks to see if the file exists, if it does NOT exist, download it and countine running.
 if not(os.path.isfile('awslog.txt')):  
     print('You do not have the file, beginning file download with urllib2...')
     url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
@@ -13,4 +14,15 @@ file = open("awslog.txt","r")
 
 # Splits file by line into a list
 read_file = file.read()
-split_file = read_file.split('\n')
+log = read_file.split('\n')
+
+# Splits file into 12 seperate monthly files
+oct_94 = open("October_1994", "w")
+for line in log:
+    if "Oct/1994" in line:
+        oct_94.write(line)
+        
+oct_94.close()
+        
+    
+
